6279faa0513105d597046462f5299c1ac57c4759
Okke Harsta <oharsta@zilverline.com>
2019-11-22 12:14:49 +0100

    Added JS jest tests

---
+++ myconext-gui/src/locale/nl.js
@@ -0,0 +1,39 @@
+import I18n from "i18n-js";
+
+I18n.translations.nl = {
+    header: {
+        title: "Mijn SURFconext"
+    },
+    landing: {
+        loginHeader: "Sign in to my.SURFconext",
+        info: "My.SURFconext provides an overview of your account settings for the Guest Identity Provider from SURF. In order to sign you will need an account which can be created on the fly.",
+        login: "Login",
+        questionsTitle: "What can you do with my.SURFconext?",
+        whatCanYouDo: "We are targeting my.SURFconext to be the home for all your personal SURFconext settings.",
+        questions: "If you have any questions you can contact <a href=\"mailto:surfconext@surfnet.nl\">surfconext@SURFnet.nl</a>"
+    },
+    notFound: {
+        main: "404 - Not Found"
+    },
+    profile: {
+        givenName: "Your given name",
+        familyName: "Your family name",
+        update: "Update",
+        updated: "Your profile has been updated"
+    },
+    security: {
+        currentPassword: "Current password",
+        newPassword: "New password",
+        confirmPassword: "Confirm new password",
+        usePassword: "Use password",
+        useMagicLink: "Use Magic Link",
+        update: "Update",
+        updated: "Your security settings have been updated",
+        passwordDisclaimer: "Make sure it's at least 15 characters OR at least 8 characters including a number and a uppercase letter."
+    },
+    home: {
+        welcome: "Welcome {{name}}",
+        profile: "Profile",
+        security: "Security"
+    }
+};
--- myconext-server/src/main/java/myconext/crypto/KeyGenerator.java
+++ myconext-server/src/main/java/myconext/crypto/KeyGenerator.java
@@ -1,28 +1,23 @@
 package myconext.crypto;
 
-import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1Primitive;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemWriter;
 
 import java.io.StringWriter;
+import java.io.Writer;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.Provider;
 import java.security.Security;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Base64;
@@ -38,78 +33,43 @@ public class KeyGenerator {
     }
 
     public String[] generateKeys() throws Exception {
-        Base64.Encoder encoder = Base64.getEncoder();
-
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
         kpg.initialize(2048);
         KeyPair kp = kpg.generateKeyPair();
+
         //PrivateKey is PKCS8 format and we need to end up in PEM format
-        PrivateKey priv = kp.getPrivate();
-        byte[] privBytes = priv.getEncoded();
-
-        PrivateKeyInfo pkInfo = PrivateKeyInfo.getInstance(privBytes);
-        ASN1Encodable encodable = pkInfo.parsePrivateKey();
-        ASN1Primitive primitive = encodable.toASN1Primitive();
-        byte[] privateKeyPKCS1 = primitive.getEncoded();
-
-//            PKCS8EncodedKeySpec keyspec = new PKCS8EncodedKeySpec(kp.getPrivate().getEncoded());
-
-        String privateKey = "-----BEGIN PRIVATE KEY-----\n";
-        privateKey += encoder.encodeToString(privateKeyPKCS1);
-        privateKey += "\n-----END PRIVATE KEY-----\n";
-
-        PemObject pemObject = new PemObject("RSA PRIVATE KEY", privateKeyPKCS1);
-        StringWriter stringWriter = new StringWriter();
-        PemWriter pemWriter = new PemWriter(stringWriter);
-        pemWriter.writeObject(pemObject);
+        Writer writer = new StringWriter();
+        JcaPEMWriter pemWriter = new JcaPEMWriter(writer);
+        pemWriter.writeObject(kp.getPrivate());
         pemWriter.close();
-        String pemString = stringWriter.toString();
 
-//            StringBuilder sb = new StringBuilder();
-//            sb.append("-----BEGIN PRIVATE KEY-----");
-//            sb.append(new String(Base64.encode(keyspec.getEncoded())));
-//            sb.append("-----END PRIVATE KEY-----");
+        String pemString = writer.toString();
+        String certificate = certificate(kp);
 
-//            PrivateKeyInfo pkInfo = PrivateKeyInfoFactory.createPrivateKeyInfo((AsymmetricKeyParameter) kp.getPrivate());
-//            String privateKey = encoder.encodeToString(pkInfo.get);
-////https://stackoverflow.com/questions/48333877/how-to-write-private-key-to-password-protected-der-format-in-java
-//            String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n";
-//            privateKey += encoder.encodeToString(kp.getPrivate().getEncoded());
-//            privateKey += "\n-----END RSA PRIVATE KEY-----\n";
-
-        Certificate certificate = selfSign(kp, "CN=test,O=Test Certificate", bcProvider);
-
-        String certificateS = "-----BEGIN CERTIFICATE-----\n";
-        certificateS += encoder.encodeToString(certificate.getEncoded());
-        certificateS += "\n-----END CERTIFICATE-----\n";
-        return new String[]{pemString, certificateS};
+        return new String[]{pemString, certificate};
     }
 
 
+    private String certificate(KeyPair keyPair) throws OperatorCreationException, CertificateException, CertIOException {
+        X500Name dnName = new X500Name("CN=test,O=Test Certificate");
+        BigInteger certSerialNumber = new BigInteger(Long.toString(System.currentTimeMillis()));
+        ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSA").build(keyPair.getPrivate());
 
-    private Certificate selfSign(KeyPair keyPair, String subjectDN, Provider bcProvider) throws OperatorCreationException, CertificateException, CertIOException {
+        JcaX509v3CertificateBuilder certBuilder =
+                new JcaX509v3CertificateBuilder(
+                        dnName,
+                        certSerialNumber,
+                        new Date(),
+                        Date.from(LocalDateTime.now().plusYears(1).toInstant(ZoneOffset.UTC)),
+                        dnName,
+                        keyPair.getPublic());
 
-        Date startDate = new Date();
+        X509Certificate certificate = new JcaX509CertificateConverter().setProvider(bcProvider).getCertificate(certBuilder.build(contentSigner));
 
-        X500Name dnName = new X500Name(subjectDN);
-        BigInteger certSerialNumber = new BigInteger(Long.toString(System.currentTimeMillis())); // <-- Using the current timestamp as the certificate serial number
-        Date endDate = Date.from(LocalDateTime.now().plusYears(1).toInstant(ZoneOffset.UTC));
-        String signatureAlgorithm = "SHA256WithRSA";
-
-        ContentSigner contentSigner = new JcaContentSignerBuilder(signatureAlgorithm).build(keyPair.getPrivate());
-
-        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(dnName, certSerialNumber, startDate, endDate, dnName, keyPair.getPublic());
-
-        // Extensions --------------------------
-
-        // Basic Constraints
-//        BasicConstraints basicConstraints = new BasicConstraints(true); // <-- true for CA, false for EndEntity
-//
-//        certBuilder.addExtension(new ASN1ObjectIdentifier("2.5.29.19"), true, basicConstraints); // Basic Constraints is usually marked as critical.
-//
-        // -------------------------------------
-
-        return new JcaX509CertificateConverter().setProvider(bcProvider).getCertificate(certBuilder.build(contentSigner));
+        String result = "-----BEGIN CERTIFICATE-----\n";
+        result += Base64.getEncoder().encodeToString(certificate.getEncoded());
+        result += "\n-----END CERTIFICATE-----\n";
+        return result;
     }
 
 }
