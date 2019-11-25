5f336e9e2b0d994a5f6f119df5581050bd7872f1
Okke Harsta <oharsta@zilverline.com>
2019-11-25 13:51:49 +0100

    Added JS regexp tests

---
+++ account-gui/src/__tests__/validation/regexp.test.js
@@ -0,0 +1,9 @@
+import {validEmail} from "../../validation/regexp";
+
+test("Valid email", () => {
+    expect(validEmail("nope")).toBe(false);
+    expect(validEmail("nope@a")).toBe(false);
+    expect(validEmail("nope@a.com")).toBe(false);
+
+    expect(validEmail("nope@aa.com")).toBe(true);
+});
