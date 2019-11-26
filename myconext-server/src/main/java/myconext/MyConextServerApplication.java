870e4bb662528a3f747ff1c434e4668b6ac56e5d
Okke Harsta <oharsta@zilverline.com>
2019-11-26 09:27:54 +0100

    Enabled scheduling for authn requests cleanup

--- myconext-server/src/main/java/myconext/MyConextServerApplication.java
+++ myconext-server/src/main/java/myconext/MyConextServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, MetricsAutoConfiguration.class })
+@EnableScheduling
 public class MyConextServerApplication {
 
     public static void main(String[] args) {
