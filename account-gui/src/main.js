bba4b9927a666dc2527e9e9bfc5440c2247df056
Okke Harsta <oharsta@zilverline.com>
2019-12-22 08:41:43 +0100

    Delete not used logo's

--- account-gui/src/main.js
+++ account-gui/src/main.js
@@ -7,21 +7,19 @@ import "./locale/en";
 import "./locale/nl";
 
 if (typeof window !== "undefined") {
-	const urlSearchParams = new URLSearchParams(window.location.search);
-	if (urlSearchParams.has("lang")) {
-		I18n.locale = urlSearchParams.get("lang");
-	} else if (Cookies.get("lang")) {
-		I18n.locale = Cookies.get("lang");
-	} else {
-		I18n.locale = navigator.language.toLowerCase().substring(0, 2);
-	}
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    if (urlSearchParams.has("lang")) {
+        I18n.locale = urlSearchParams.get("lang");
+    } else if (Cookies.get("lang")) {
+        I18n.locale = Cookies.get("lang");
+    } else {
+        I18n.locale = navigator.language.toLowerCase().substring(0, 2);
+    }
 
 } else {
-	I18n.locale = "en";
+    I18n.locale = "en";
 }
 
-const app = new App({
-	target: document.body,
-});
+const app = new App({target: document.body});
 
 export default app;
