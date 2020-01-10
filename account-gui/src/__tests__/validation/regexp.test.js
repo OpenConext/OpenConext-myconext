019dac6b14d8a9f596390cb6c5245b92a6598d7d
Okke Harsta <oharsta@zilverline.com>
2020-01-10 13:57:19 +0100

    Fixed regexp tests

--- account-gui/src/__tests__/validation/regexp.test.js
+++ account-gui/src/__tests__/validation/regexp.test.js
@@ -3,7 +3,8 @@ import {validEmail} from "../../validation/regexp";
 test("Valid email", () => {
     expect(validEmail("nope")).toBe(false);
     expect(validEmail("nope@a")).toBe(false);
-    expect(validEmail("nope@a.com")).toBe(false);
+    expect(validEmail("nope@a#$%")).toBe(false);
 
+    expect(validEmail("nope@a.com")).toBe(true);
     expect(validEmail("nope@aa.com")).toBe(true);
 });
