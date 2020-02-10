f44fcf37614383cf27de75f0cab566328f363843
Okke Harsta <oharsta@zilverline.com>
2020-02-10 11:01:00 +0100

    Forgot to add OpenRedirectException.java

---
+++ myconext-server/src/main/java/myconext/exceptions/OpenRedirectException.java
@@ -0,0 +1,12 @@
+package myconext.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class OpenRedirectException extends RuntimeException {
+
+    public OpenRedirectException(String message) {
+        super(message);
+    }
+}
