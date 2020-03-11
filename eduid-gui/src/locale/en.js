caa3e44bbe7db25ea3715b2080d05934403854e7
Arnout <arnoutt@users.noreply.github.com>
2020-03-11 12:06:57 +0100

    Text improvements
    Text improvements

--- eduid-gui/src/locale/en.js
+++ eduid-gui/src/locale/en.js
@@ -5,14 +5,13 @@ I18n.translations.en = {
         mine: "My eduID"
     },
     public: {
-        header: "Check-in and check-out with one educational identity",
-        info: "<p> An eduID is an account for higher education in the Netherlands. </p>" +
-            "<p class='divider'> It is yours and independent of an educational institution. </p>" +
-            "<p class='divider'> You will soon be able to use it at institutions other than the one you are currently studying. You can also continue to use it after your studies, for example as an alumnus or if you are going to study elsewhere in the future. </p>" +
-            "<p class='divider'> eduID is still under development. It can currently be used for guest access on SURFconext as a replacement for OneGini. </p>",
+        header: "eduID: 1 guest account for education and research",
+        info: "<p>An eduID is an account for guest users within education and research in the Netherlands. It is yours and exists independent of an educational institution.</p>" +
+            "<p class='divider'>You can use eduID to login as a guest user to several services connected to <a href=\"https://surfconext.nl\" target=\"_blank\">SURFconext</a>. A guest user is someone who doesn't have an account with one of the connected educational institutions.</p>" +
+            "<p class='divider'>eduID is still under development. In the future, you will be able to use eduID for many more things other than guest usage in SURFconext. For example, you can use eduID after your studies, as an alumnus, or during your studies, if you wish to follow classes at several institutions simultaneously.</p>",
         request: {
             header: "Request",
-            info: "You only need a email address.",
+            info: "You only need an e-mail address.",
             request: "Request an eduID"
         },
     },
@@ -21,7 +20,7 @@ I18n.translations.en = {
         title2: "Something went wrong (404)."
     },
     footer: {
-        privacy: "Privacy policy",
+        privacy: "Privacy Policy",
         terms: "Terms of Use",
         help: "Help",
         poweredBy: "Powered by"
