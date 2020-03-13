0963a258dcd0b3707fec65dac888fb993dfea718
Okke Harsta <oharsta@zilverline.com>
2020-03-13 11:41:52 +0100

    Version 2.0.2 of eduID

--- eduid-gui/src/locale/en.js
+++ eduid-gui/src/locale/en.js
@@ -5,10 +5,10 @@ I18n.translations.en = {
         mine: "My eduID"
     },
     public: {
-        header: "eduID: 1 guest account for education and research",
-        info: "<p>An eduID is an account for guest users within education and research in the Netherlands. It is yours and exists independent of an educational institution.</p>" +
-            "<p class='divider'>You can use eduID to login as a guest user to several services connected to <a href=\"https://surfconext.nl\" target=\"_blank\">SURFconext</a>. A guest user is someone who doesn't have an account with one of the connected educational institutions.</p>" +
-            "<p class='divider'>eduID is still under development. In the future, you will be able to use eduID for many more things other than guest usage in SURFconext. For example, you can use eduID after your studies, as an alumnus, or during your studies, if you wish to follow classes at several institutions simultaneously.</p>",
+        header: "eduID: 1 account for education and research",
+        info: "<p>An eduID is an account for users within education and research in the Netherlands. It is yours and exists independent of an educational institution.</p>" +
+            "<p class='divider'>You can use eduID to login to several services connected to <a href=\"https://surfconext.nl\" target=\"_blank\">SURFconext</a>.  eduID is meant for users who do not have an account with an institution or for services where an institution-independent account is useful.</p>" +
+            "<p class='divider'>eduID is still under development. In the future, you will be able to use eduID for many more things. For example, you can use eduID after your studies, as an alumnus, or during your studies, if you wish to follow classes at several institutions simultaneously.</p>",
         request: {
             header: "Request",
             info: "You only need an e-mail address.",
--- eduid-gui/src/locale/nl.js
+++ eduid-gui/src/locale/nl.js
@@ -5,10 +5,10 @@ I18n.translations.nl = {
         mine: "Mijn eduID"
     },
     public: {
-        header: "eduID: 1 gastaccount voor onderwijs en onderzoek",
-        info: "<p>Een eduID is een account voor gastgebruikers binnen onderwijs en onderzoek in Nederland. Het is van jou, en bestaat onafhankelijk van een onderwijsinstelling.</p>" +
-            "<p class='divider'>Je kunt eduID gebruiken om als gast in te loggen op diverse diensten aangesloten op <a href=\"https://surfconext.nl\" target=\"_blank\">SURFconext</a>. Een gastgebruiker is iemand die geen account heeft bij 1 van de aangesloten instellingen.</p>" +
-            "<p class='divider'>eduID is nog volop in ontwikkeling. In de toekomst kun je eduID gebruiken voor nog veel meer dingen dan alleen gasttoegang via SURFconext. Zo kun je eduID bijvoorbeeld gebruiken na je studie, als alumnus, of tijdens je studie, als je bij meerdere instellingen tegelijk onderwijs wilt volgen.</p>",
+        header: "eduID: 1 account voor onderwijs en onderzoek",
+        info: "<p>Een eduID is een account voor gebruikers binnen onderwijs en onderzoek in Nederland. Het is van jou, en bestaat onafhankelijk van een onderwijsinstelling.</p>" +
+            "<p class='divider'>Je kunt eduID gebruiken om in te loggen op diverse diensten aangesloten op <a href=\"https://surfconext.nl\" target=\"_blank\">SURFconext</a>. eduID is vooral bedoeld voor gebruikers die geen account van een instelling hebben of voor diensten waarbij een instellingsonafhankelijk account handig is.</p>" +
+            "<p class='divider'>eduID is nog volop in ontwikkeling. In de toekomst kun je eduID gebruiken voor nog veel meer dingen. Zo kun je eduID bijvoorbeeld gebruiken na je studie, als alumnus, of tijdens je studie, als je bij meerdere instellingen tegelijk onderwijs wilt volgen.</p>",
         request: {
             header: "Aanvragen",
             info: "Je hebt alleen een e-mailadres nodig.",
