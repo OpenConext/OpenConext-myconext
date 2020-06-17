47a267623e745503644c5ef160d1ca1a0818b55f
Okke Harsta <oharsta@zilverline.com>
2020-06-17 15:25:30 +0200

    Test computeEduIdForServiceProviderIfAbsent

--- myconext-server/src/test/java/myconext/model/UserTest.java
+++ myconext-server/src/test/java/myconext/model/UserTest.java
@@ -6,6 +6,9 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +22,15 @@ public class UserTest {
 
         List<LinkedAccount> linkedAccountsSorted = user.linkedAccountsSorted();
         assertTrue(linkedAccountsSorted.get(0).getExpiresAt().toInstant().isAfter(linkedAccountsSorted.get(1).getExpiresAt().toInstant()));
+    }
+
+    @Test
+    public void computeEduIdForServiceProviderIfAbsent() {
+        User user = user();
+        String eduId = user.computeEduIdForServiceProviderIfAbsent("http://test.sp").get();
+        boolean matches = Pattern.compile("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$").matcher(eduId).matches();
+        assertTrue(matches);
+
     }
 
     private User user() {
