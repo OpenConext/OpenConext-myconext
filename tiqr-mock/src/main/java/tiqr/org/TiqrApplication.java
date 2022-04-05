41ce25321b61a4886cd136307fb63fb2916c107b
Okke Harsta <oharsta@zilverline.com>
2022-04-05 13:03:07 +0200

    No security in tiqr-mock

--- tiqr-mock/src/main/java/tiqr/org/TiqrApplication.java
+++ tiqr-mock/src/main/java/tiqr/org/TiqrApplication.java
@@ -1,9 +1,11 @@
 package tiqr.org;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ManagementWebSecurityAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class TiqrApplication {
 
 	public static void main(String[] args) {
