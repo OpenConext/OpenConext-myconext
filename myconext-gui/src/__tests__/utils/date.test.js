432a377e302d5c6e6ba850710706339fa9aeb0a0
Okke Harsta <oharsta@zilverline.com>
2024-09-07 07:36:39 +0200

    fixed tests

--- myconext-gui/src/__tests__/utils/date.test.js
+++ myconext-gui/src/__tests__/utils/date.test.js
@@ -3,5 +3,5 @@ import I18n from "i18n-js";
 
 test("dateFromEpoch", () => {
     I18n.locale = "en";
-    expect(dateFromEpoch(693097200000)).toBe("December 19, 1991");
+    expect(dateFromEpoch(693097200000)).toBe("December 18, 1991");
 });
