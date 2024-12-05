8f60b225e464ffabeb28ad689a6f536c4b129069
Okke Harsta <oharsta@zilverline.com>
2024-12-05 15:59:06 +0100

    Start for #606

---
+++ myconext-server/src/main/java/myconext/cron/InactivityMail.java
@@ -0,0 +1,8 @@
+package myconext.cron;
+
+public class InactivityMail {
+
+    //TODO, keep track of the mails send bu using an enum indicating the level of inactivity
+    //In the variables for the mail separate the NL variable and EN variable and use
+    //different variables in the mail templates
+}
