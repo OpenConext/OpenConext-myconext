8ec887fa0dc92a5fe5d34123ce41076dab954ec5
Okke Harsta <oharsta@zilverline.com>
2025-01-10 11:36:35 +0100

    Upped to open-saml 5

--- myconext-server/src/main/java/myconext/security/SecurityConfiguration.java
+++ myconext-server/src/main/java/myconext/security/SecurityConfiguration.java
@@ -241,7 +241,7 @@ public class SecurityConfiguration {
                             AbstractPreAuthenticatedProcessingFilter.class)
                     .authorizeHttpRequests(auth -> auth
                             .anyRequest().hasRole("GUEST"));
-            if (environment.acceptsProfiles(Profiles.of("test", "test2", "dev"))) {
+            if (environment.acceptsProfiles(Profiles.of("test", "dev"))) {
                 //we can't use @Profile, because we need to add it before the real filter
                 http.addFilterBefore(new MockShibbolethFilter(), ShibbolethPreAuthenticatedProcessingFilter.class);
             }
