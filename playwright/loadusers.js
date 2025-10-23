ec6c591edc9a55b6f171c34ea6fc79723fb1ca63
Peter Havekes <peter@havekes.eu>
2025-10-23 14:56:55 +0200

    Add users to mongodb

---
+++ playwright/loadusers.js
@@ -0,0 +1,11 @@
+const filePath = "../myconext-server/src/test/resources/users.json";
+
+try {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const users = JSON.parse(fileContent);
+    const result = db.users.insertMany(users);
+
+    print(`Successfully inserted ${result.insertedCount} documents into the 'users' collection.`);
+} catch (e) {
+    print(`Error loading or inserting data: ${e.message}`);
+}
