3ce235a5d8bf2d456816390fb16a04c48b445510
Ines <Tyskai@users.noreply.github.com>
2025-10-23 15:28:31 +0200

    login test verbeteren

--- playwright/tests/login.spec.ts
+++ playwright/tests/login.spec.ts
@@ -1,8 +1,18 @@
 import { test, expect } from '@playwright/test';
-import { loginNL } from './login';
 
 test('Inloggen op test.eduid.nl in het Nederlands', async ({ page }) => {
-  await login(page);
+  await page.goto('http://localhost:3002/');
+  await page.getByRole('link', { name: 'EN' }).click();
+  await page.getByRole('button', { name: 'My eduID' }).click();
+  await page.getByRole('link', { name: 'EN' }).click();
+  await page.getByRole('textbox', { name: 'e.g. user@gmail.com' }).click();
+  await page.getByRole('textbox', { name: 'e.g. user@gmail.com' }).fill('jdoe@example.com');
+  await page.getByRole('link', { name: 'Next' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('secret');
+  await page.getByRole('link', { name: 'Login', exact: true }).click();
+  await page.waitForURL('http://localhost:3001/');
+  await expect(page.getByRole('heading', { name: 'Hi Johnny!' })).toBeVisible();
   await expect(page.getByText('Manage your personal info,')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Personal info' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Data & activity' })).toBeVisible();
