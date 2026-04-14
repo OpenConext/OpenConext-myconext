29f4a3646154bf4d7346aeca1ac99584e3b13014
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2026-04-14 16:01:18 +0200

    #542 Providing data for EduIDOidcUserService

--- myconext-server/src/main/java/myconext/security/EduIDOidcUserService.java
+++ myconext-server/src/main/java/myconext/security/EduIDOidcUserService.java
@@ -1,13 +1,18 @@
 package myconext.security;
 
 import jakarta.servlet.http.HttpServletRequest;
+import myconext.manage.Manage;
 import myconext.model.ExternalUser;
 import myconext.model.User;
+import myconext.repository.ExternalUserRepository;
 import myconext.repository.UserRepository;
 import oidc.security.CustomOidcUserService;
 import oidc.security.UserProvisioning;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -21,18 +26,49 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import static myconext.log.MDCContext.logWithContext;
 
 public class EduIDOidcUserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
 
     private static final Log LOG = LogFactory.getLog(EduIDOidcUserService.class);
 
+    private final Environment environment;
+    private final Manage manage;
+    private final UserRepository userRepository;
+    private final ExternalUserRepository externalUserRepository;
     private final OidcUserService delegate;
+    private final String mijnEduIDEntityId;
+    private final String mijnEduIDHost;
+    private final String serviceDeskHost;
+    private  final String activeHost;
+    private final List<String> serviceDeskRoles;
 
-    public EduIDOidcUserService(UserRepository userRepository) {
+    public EduIDOidcUserService(
+            Environment environment,
+            Manage manage,
+            UserRepository userRepository,
+            ExternalUserRepository externalUserRepository,
+            String mijnEduIDEntityId,
+            String mijnEduIDHost,
+            String serviceDeskHost,
+            String activeHost,
+            List<String> serviceDeskRoles
+    ) {
         //This is the default
+        this.environment = environment;
+        this.manage = manage;
+        this.userRepository = userRepository;
+        this.externalUserRepository = externalUserRepository;
         this.delegate = new OidcUserService();
+        this.mijnEduIDEntityId = mijnEduIDEntityId;
+        this.mijnEduIDHost = mijnEduIDHost;
+        this.serviceDeskHost = serviceDeskHost;
+        this.activeHost = activeHost;
+        this.serviceDeskRoles = serviceDeskRoles;
     }
 
     @Override
@@ -45,33 +81,44 @@ public class EduIDOidcUserService implements OAuth2UserService<OidcUserRequest, 
 
         LOG.debug("Provision oidcUser: " + claims);
 
-//        Get from claims
-
-//        String uid = claims.get("sub")
-//        String schacHomeOrganization = getHeader(SHIB_SCHAC_HOME_ORGANIZATION, request);
-//        String email = getHeader(SHIB_EMAIL, request);
-//        String givenName = getHeader(SHIB_GIVEN_NAME, request);
-//        String familyName = getHeader(SHIB_SUR_NAME, request);
+        // Get from claims
+        String uid = claims.get("sub").toString();
+        String schacHomeOrganization = claims.get("schac_home_organization").toString(); //getHeader(SHIB_SCHAC_HOME_ORGANIZATION, request);
+        String email = claims.get("email_verified").toString(); //getHeader(SHIB_EMAIL, request);
+        String givenName = claims.get("given_name").toString(); //getHeader(SHIB_GIVEN_NAME, request);
+        String familyName = claims.get("family_name").toString(); //getHeader(SHIB_SUR_NAME, request);
 
         //Now provision the user orretrieve the user
+        String host;
+        if (environment.acceptsProfiles(Profiles.of("test", "dev"))) {
+            host = activeHost;
+        } else {
+            host = getHeader("host");
+        }
 
-        String host = getHeader("host");
-//        if (!StringUtils.hasText(host)) {
-//            throw new IllegalArgumentException("There is no host header in the request");
-//        }
-//        boolean logInToEduID = host.toLowerCase().equals(this.mijnEduIDHost);
-//        boolean logInToServiceDesk = host.toLowerCase().equals(this.serviceDeskHost);
-//        Optional<User> optionalUser = Optional.empty();
-//        Optional<ExternalUser> optionalExternalUser = Optional.empty();
-//
-//        if (logInToEduID) {
-//            optionalUser = userRepository.findUserByUid(uid);
-//        } else if (logInToServiceDesk) {
-//            optionalExternalUser = externalUserRepository.findUserByUid(uid);
-//        } else {
-//            throw new IllegalArgumentException("Unknown host header in the request: " + host);
-//        }
-            //TODO add the ID of the usesr (either existing or new user) to the claims
+        if (!StringUtils.hasText(host)) {
+            throw new IllegalArgumentException("There is no host header in the request");
+        }
+        boolean logInToEduID = host.toLowerCase().equals(this.mijnEduIDHost);
+        boolean logInToServiceDesk = host.toLowerCase().equals(this.serviceDeskHost);
+        Optional<User> optionalUser = Optional.empty();
+        Optional<ExternalUser> optionalExternalUser = Optional.empty();
+
+        if (logInToEduID) {
+            optionalUser = userRepository.findUserByUid(uid);
+        } else if (logInToServiceDesk) {
+            optionalExternalUser = externalUserRepository.findUserByUid(uid);
+        } else {
+            throw new IllegalArgumentException("Unknown host header in the request: " + host);
+        }
+
+
+
+        // Any of the 2 types of users is fetched -- see the provision step in Shib filter
+        // The endgame is to return a OidcUser at all times
+
+        // TODO add the ID of the usesr (either existing or new user) to the claims
+//        newClaims.put("sub", uid);
         OidcUserInfo oidcUserInfo = new OidcUserInfo(newClaims);
         oidcUser = new DefaultOidcUser(oidcUser.getAuthorities(), oidcUser.getIdToken(), oidcUserInfo);
         return oidcUser;
@@ -90,5 +137,28 @@ public class EduIDOidcUserService implements OAuth2UserService<OidcUserRequest, 
         ServletRequestAttributes attrs = (ServletRequestAttributes)
                 RequestContextHolder.currentRequestAttributes();
         return attrs.getRequest();
+    }
+
+    private User provisionUser(String uid, String schacHomeOrganization, String givenName, String familyName,
+                               String email, String preferredLanguage) {
+        User user = new User(uid, email, givenName, givenName, familyName, schacHomeOrganization,
+                preferredLanguage, mijnEduIDEntityId, manage);
+        user.setNewUser(false);
+        user = userRepository.save(user);
+
+        logWithContext(user, "add", "user", LOG, String.format("Provisioned new user %s", user.getEmail()));
+
+        return user;
+    }
+
+    private ExternalUser provisionServiceDeskUser(String uid, String schacHomeOrganization, String givenName, String familyName,
+                                                  String email, List<String> memberships) {
+        ExternalUser user = new ExternalUser(uid, email, givenName, familyName, schacHomeOrganization);
+        boolean isServiceDeskMember = this.serviceDeskRoles.stream().anyMatch(memberships::contains);
+        user.setServiceDeskMember(isServiceDeskMember);
+        user.setNewUser(false);
+        user = externalUserRepository.save(user);
+
+        return user;
     }
 }
