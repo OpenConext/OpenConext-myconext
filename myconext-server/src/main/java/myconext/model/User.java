89374c5c1806a833a4dc8aaf77669af7f6097be6
Okke Harsta <oharsta@zilverline.com>
2026-06-10 12:31:48 +0200

    Fixes  #1219

--- myconext-server/src/main/java/myconext/model/User.java
+++ myconext-server/src/main/java/myconext/model/User.java
@@ -135,6 +135,7 @@ public class User implements Serializable, UserDetails {
         this.chosenName = (String) userInfo.get("given_name");
         this.givenName = (String) userInfo.get("given_name");
         this.familyName = (String) userInfo.get("family_name");
+        this.lastLogin = System.currentTimeMillis();
     }
 
     public User(String uid, String email, String chosenName, String givenName, String familyName,
@@ -152,6 +153,7 @@ public class User implements Serializable, UserDetails {
         }
         this.newUser = true;
         this.created = System.currentTimeMillis() / 1000L;
+        this.lastLogin = System.currentTimeMillis();
     }
 
     public User(String uid, String email, String chosenName, String givenName, String familyName,
