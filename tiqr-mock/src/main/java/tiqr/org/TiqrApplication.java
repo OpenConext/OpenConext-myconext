6e9b80374d0600a7d93dc9f1281d35b6e48c5074
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2026-09-02 06:38:59 +0200

    Fixes #1312 - fix account creating on mfa (#1322)
    * #1312 Removes Spring Security from TiqrMock
    
    * #1312 Removes hiding condition for mfa

--- tiqr-mock/src/main/java/tiqr/org/TiqrApplication.java
+++ tiqr-mock/src/main/java/tiqr/org/TiqrApplication.java
@@ -1,11 +1,9 @@
 package tiqr.org;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {ManagementWebSecurityAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class TiqrApplication {
 
 	public static void main(String[] args) {
